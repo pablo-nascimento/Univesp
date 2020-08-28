7ba1ef5dd7665dffe0acc9f3555dd37f131599fe
pablo-nascimento <pablonascimentoaraujo@hotmail.com>
2020-08-27 21:51:58 -0300

    Bacharelado em Ciencia de Dados - Univesp

---
+++ univesp/ExpAritm01.py
@@ -0,0 +1,33 @@
+#Algoritmo e Programação de Computadores I
+#Semana 02
+#Expressões Aritméticas e Operadores
+## // - divisão inteira - pega só o quociente, não considerando o resto
+## 3//2 - 1 e resta 1 (quociente da divisão)
+## % módulo - resto da divisão 3 % 2 = 1
+
+print('5/2:', 5/2)
+print('Precedencia entre () - 4/(2+3):', 4/(2+3))
+print('3/2:', 3/2)
+
+#Divisão inteira, faz a divisão e pega a parte inteira = 3 // 2 = 1
+print('Quociente da divisão - 3//2:', 3//2)
+
+#Módulo - pega o resto da divisão.
+print('Módulo ou resto da divisão - 3%2:',3%2)
+
+#Potência
+print('Potência de 2 a 3 = ', 2**3)
+
+#Operadores com a mesma precedência - faz da esquerda para a direita
+print('Sinais em precedencia - 2-4+1:',2-4+1)
+
+#Abs - retorna o valor absoluto
+print('Abs de -2:', abs(-2))
+print('Mínimo de 5,7,3,1 - min():', min(5,7,3,1))
+print('Máximo de 5,7,3,1 - max():', max(5,7,3,1))
+
+#3.6, 8.9, 10
+media = 3-4/((1/3.6)+(1/8.9)+(1/10))
+print('Média Harmonica de 3.6 - 8.9 e 10: ', media)
+
+
